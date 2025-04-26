97bfd3504f3e7b746b49f871b2581333152d5b2c
youssef <youssef.rhandoumi@gmail.com>
2025-04-26 16:27:28 +0100

    remade token counter and prototyping token slicer

--- main.c
+++ main.c
@@ -6,57 +6,64 @@
 /*   By: yrhandou <yrhandou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 12:34:48 by yrhandou          #+#    #+#             */
-/*   Updated: 2025/04/21 19:39:13 by yrhandou         ###   ########.fr       */
+/*   Updated: 2025/04/26 16:26:52 by yrhandou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "coreutils/libft.h"
 
-size_t token_counter(char *s)
+int ft_is_operator(int c)
 {
-	size_t i;
-	size_t j;
-	size_t counter;
-	char * delimiters;
-
-	delimiters = "";
-	int *delimiters_counter;
-
-	delimiters_counter = malloc(sizeof(int));
-	if(delimiters)
-		return 0;
-	char *str;
-	i = 0;
-	j = 0;
-	counter = 0;
-	str = s;
-	if (str[0] == ' ')
-		str++;
-	str = ft_strchr(str, ' ');
-	if (!str)
-		return 0;
-	while (str)
-	{
-
-		counter++;
-		str++;
-		str = ft_strchr(str, ' ');
-	}
-	free(delimiters);
-	return counter;
+	if (c == '|' || c  == '>') // || c == '||' || c == '>>'|| c == '<<'|| c == '&&')
+		return 1;
+	return 0;
 }
+
+
+
+// size_t token_counter(char *s)
+// {
+// 	size_t i;
+// 	size_t j;
+// 	size_t counter;
+// 	char *delimiters;
+// 	int *delimiters_counter;
+
+// 	delimiters_counter = malloc(sizeof(int));
+// 	if (delimiters)
+// 		return 0;
+// 	char *str;
+// 	i = 0;
+// 	j = 0;
+// 	counter = 0;
+// 	str = s;
+// 	if (str[0] == ' ')
+// 		str++;
+// 	str = ft_strchr(str, ' ');
+// 	if (!str)
+// 		return 0;
+// 	while (str)
+// 	{
+
+// 		counter++;
+// 		str++;
+// 		str = ft_strchr(str, ' ');
+// 	}
+// 	free(delimiters);
+// 	return counter;
+// }
 
 // int lexer(char *str)
 // {
-	// 	size_t i;
-	// 	size_t j;
-	// 	t_token *tokens;
+// 	size_t i;
+// 	size_t j;
+// 	t_token *tokens;
 // 	size_t token_count;
 // 	token_count = 0;
-	// tokens = malloc(sizeof(t_token));
-	// if(!tokens)
-	// 	return 0;
+// tokens = malloc(sizeof(t_token));
+// if(!tokens)
+// 	return 0;
 // 	i = 0;
 // 	j = 0;
 // 	char **sub_tokens = ft_split(str, ' ');
@@ -77,7 +84,7 @@ size_t token_counter(char *s)
 // 		i++;
 // 	}
 // 	i++;
-// 	token_count++; 
+// 	token_count++;
 // 	if (str[i] == '-')
 // 	{
 // 		while (ft_isalpha(str[i]))
@@ -91,9 +98,52 @@ size_t token_counter(char *s)
 // 	return i;
 // }
 
+int token_counter(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char *store;
+	if (!str)
+		return NULL;
+	int token_count = 1;
+	while (str[i])
+	{
+		if (ft_is_operator(str[i]))
+		{
+			token_count++;
+			if (str[i + 1] != '\0')
+				token_count++;
+		}
+		i++;
+	}
+	return token_count;
+}
+char **token_slicer(char *str, int token_count)
+{
+	int i = 0;
+	int j = 0;
+	char *store;
+	if (!str)
+		return NULL;
+	char **token = malloc(token_count * sizeof(int));
+	if(!token)
+		return NULL;
+	while (str[i])
+	{
+		if (ft_is_operator(str[i]))
+		{
+			token[j] = ft_substr(str, 0,i);
+		}
+		i++;
+	}
+	return token_count;
+}
+
 int main(void)
 {
-	char *str;
-
-	printf("%zu\n", token_counter(" ls -al | grep '.' > txtfile"));
+	int i =0;
+	int j =0;
+	char *str = "ls - al";
+	int token_count=  token_counter(str);
+	printf("%d", tokenizer(str));
 }
