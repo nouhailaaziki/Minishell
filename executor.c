9a2d012ffba2b810c5c49fd352b5d196d4a007d6
Nouhaila Aziki <noaziki@e3r10p1.1337.ma>
2025-05-17 10:55:20 +0100

    merge

--- executor.c
+++ executor.c
@@ -6,7 +6,7 @@
 /*   By: noaziki <noaziki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 16:37:12 by noaziki           #+#    #+#             */
-/*   Updated: 2025/05/12 12:27:36 by noaziki          ###   ########.fr       */
+/*   Updated: 2025/05/13 11:37:09 by noaziki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,21 @@ void	executor(char **argv, int argc, t_env **env_list)
 	{
 		if (ft_strcmp("env", argv[1]) == 0
 			|| ft_strcmp("ENV", argv[1]) == 0)
-			run_env(env_list);
+			last_status = run_env(env_list);
 		if (ft_strcmp("echo", argv[1]) == 0
 			|| ft_strcmp("ECHO", argv[1]) == 0)
-			run_echo(argv);
-		if (ft_strcmp("exit", argv[1]) == 0)
-			run_exit(argv, argc, last_status, env_list);
+			last_status = run_echo(argv);
 		if (ft_strcmp("cd", argv[1]) == 0
 			|| ft_strcmp("CD", argv[1]) == 0)
-			run_cd(argv, env_list);
+			last_status = run_cd(argv, env_list);
 		if (ft_strcmp("pwd", argv[1]) == 0
 			|| ft_strcmp("PWD", argv[1]) == 0)
-			run_pwd(env_list);
+			last_status = run_pwd(env_list);
 		if (ft_strcmp("unset", argv[1]) == 0)
-			run_unset(env_list, argv);
+			last_status = run_unset(env_list, argv);
 		if (ft_strcmp("export", argv[1]) == 0)
-			run_export(argc, argv, env_list);
+			last_status = run_export(argc, argv, env_list);
+		if (ft_strcmp("exit", argv[1]) == 0)
+			run_exit(argv, argc, last_status, env_list);
 	}
 }
