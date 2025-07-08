f9c21cb5412dd3042fd04de4a4e6b40253f8b9cd
Nouhaila Aziki <noaziki@e3r3p4.1337.ma>
2025-07-08 13:46:22 +0100

    fix an issue in wildcards , still segfault in one case and double free

--- execution/wildcards/handle_wildcards.c
+++ execution/wildcards/handle_wildcards.c
@@ -6,7 +6,7 @@
 /*   By: noaziki <noaziki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 23:15:34 by noaziki           #+#    #+#             */
-/*   Updated: 2025/07/08 10:42:07 by noaziki          ###   ########.fr       */
+/*   Updated: 2025/07/08 13:44:55 by noaziki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,20 +41,25 @@ size_t	match_pattern(const char *pattern, const char *string)
 	return (*string == '\0');
 }
 
-void	handle_wildcards(t_tree *cmd_node, \
-const char *pattern, size_t arg_index, char *pwd)
+void	handle_wildcards(t_tree *cmd_node, const char *pattern, \
+size_t arg_index, char *pwd)
 {
 	DIR				*dir;
 	struct dirent	*entry;
-	size_t			matches_capacity;
 	char			**matches;
 	size_t			matches_count;
-	char			**new_matches;
-	size_t			new_argc;
+	size_t			matches_capacity;
 	char			**new_argv;
+	size_t			new_argc;
 	size_t			current_pos;
 	size_t			i;
+	char			**new_matches;
 
+	matches = NULL;
+	matches_count = 0;
+	matches_capacity = 10;
+	new_argv = NULL;
+	new_argc = 0;
 	dir = opendir(pwd);
 	if (!dir)
 	{
@@ -62,19 +67,21 @@ const char *pattern, size_t arg_index, char *pwd)
 		free(pwd);
 		return ;
 	}
-	matches_capacity = 10;
 	matches = nalloc(matches_capacity * sizeof(char *));
 	if (!matches)
 	{
 		closedir(dir);
 		free(pwd);
-		return (perror("malloc"));
+		return ;
 	}
-	matches_count = 0;
-	while ((entry = readdir(dir)) != NULL)
+	entry = readdir(dir);
+	while (entry)
 	{
 		if (entry->d_name[0] == '.' && pattern[0] != '.')
+		{
+			entry = readdir(dir);
 			continue ;
+		}
 		if (match_pattern(pattern, entry->d_name))
 		{
 			if (matches_count >= matches_capacity)
@@ -84,23 +91,28 @@ const char *pattern, size_t arg_index, char *pwd)
 				if (!new_matches)
 				{
 					perror("malloc");
-					closedir(dir);
-					free(pwd);
-					return ;
+					break ;
+				}
+				i = 0;
+				while (i < matches_count)
+				{
+					new_matches[i] = matches[i];
+					i++;
 				}
 				matches = new_matches;
 			}
 			matches[matches_count++] = na_strdup(entry->d_name);
 		}
+		entry = readdir(dir);
 	}
 	closedir(dir);
 	free(pwd);
 	if (matches_count == 0)
 		return ;
-	new_argc = cmd_node->argc - 1 + matches_count;
+	new_argc = na_arrlen(cmd_node->cmd) - 1 + matches_count;
 	new_argv = nalloc((new_argc + 1) * sizeof(char *));
 	if (!new_argv)
-		return (perror("malloc"));
+		return ;
 	current_pos = 0;
 	i = 0;
 	while (i < arg_index)
@@ -115,40 +127,54 @@ const char *pattern, size_t arg_index, char *pwd)
 		i++;
 	}
 	i = arg_index + 1;
-	while (i < cmd_node->argc)
+	while (cmd_node->cmd[i])
 	{
 		new_argv[current_pos++] = na_strdup(cmd_node->cmd[i]);
 		i++;
 	}
+	new_argv[current_pos] = NULL;
 	cmd_node->cmd = new_argv;
-	cmd_node->argc = new_argc;
 }
 
 void	check_for_wildcards(t_tree *cmd_node, t_stash *stash)
 {
 	size_t	i;
-	size_t	original_argc;
 	char	*pwd;
+	int		original_len;
 
 	i = 0;
 	pwd = getcwd(NULL, 0);
 	if (!pwd)
-	{
-		pwd = na_strdup(stash->pwd_backup);
 		if (!pwd)
-			return (perror("malloc"));
-	}
+			return (perror("getcwd"));
 	if (!cmd_node || !cmd_node->cmd)
+	{
+		free(pwd);
 		return ;
+	}
 	while (cmd_node->cmd[i])
 	{
 		if (ft_strchr(cmd_node->cmd[i], '*'))
 		{
-			original_argc = cmd_node->argc;
+			original_len = na_arrlen(cmd_node->cmd);
 			handle_wildcards(cmd_node, cmd_node->cmd[i], i, pwd);
-			if (cmd_node->argc != original_argc)
-				i = -1;
+			if (na_arrlen(cmd_node->cmd) != original_len)
+			{
+				i = 0;
+				pwd = getcwd(NULL, 0);
+				if (!pwd)
+				{
+					pwd = na_strdup(stash->pwd_backup);
+					if (!pwd)
+					{
+						perror("malloc");
+						return ;
+					}
+				}
+				continue ;
+			}
 		}
 		i++;
 	}
+	free(pwd);
 }
